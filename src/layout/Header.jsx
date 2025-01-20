77e466e3aaba2a0df608a4514f5b814fff857f65
Hesam Pirzadeh <pirzadehhesam00@gmail.com>
2025-01-20 21:45:33 +0330

    layout

---
+++ src/layout/Header.jsx
@@ -0,0 +1,12 @@
+
+import styles from "../css/Header.module.css"
+
+function Header() {
+  return (
+    <div className={styles.header}>
+      <p>sssss</p>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/layout/Layout.jsx
@@ -0,0 +1,17 @@
+
+import Footer from './Footer'
+import Header from './Header'
+
+
+
+function Layout({children}) {
+  return (
+    <div>
+        <Header/>
+        {children}
+        <Footer/>
+    </div>
+  )
+}
+
+export default Layout
